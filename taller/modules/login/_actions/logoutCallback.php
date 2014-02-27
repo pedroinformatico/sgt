2493b81c484894ffb16bad1a69de95d626daa704
andresplaza <andresplaza@192.168.0.178>
2014-02-27 16:52:39 -0400

    método para obtener cliente según parámetros

--- taller/modules/login/_actions/logoutCallback.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!isset($_SESSION)) {
-    session_start();
-}
-require_once $_SESSION['__BASESERVER__'].'/modules/login/_actions/_validateUserPermissions.php';
-echo json_encode(logOut());
-?>
