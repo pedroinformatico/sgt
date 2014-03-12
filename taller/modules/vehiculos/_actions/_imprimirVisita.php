fa10a1c26202d6987ab31f71436500a47f662336
andresplazac <andres.plaza.c@gmail.com>
2014-03-12 06:57:49 -0300

    carga impresión ficha

--- taller/modules/vehiculos/_actions/_imprimirVisita.php
+++ taller/modules/vehiculos/_actions/_imprimirVisita.php
@@ -1,8 +1,9 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+if (!isset($_SESSION)) {
+    session_start();
+}
+require_once '_functions.php';
 
+$idVisita = isset($_REQUEST["idVisita"]) ? $_REQUEST["idVisita"] : "";
+echo json_encode(obtenerDatosVisitaImprimir($idVisita));
--- taller/modules/vehiculos/js/visitaImprimir.class.js
+++ taller/modules/vehiculos/js/visitaImprimir.class.js
@@ -12,7 +12,9 @@
         actions: {
             abrirFichaImpresion: function(id) {
                 $("#dialogResumenVisita").dialog("open");
+                visitaImprimir.callbacks.limpiarFichaImpresion();
                 visitaImprimir.actions.obtenerNotasPorVisita(id);
+                visitaImprimir.actions.obtenerDatosVisita(id);
                 //Logica para realizar las peticiones
             },
             obtenerNotasPorVisita: function(idVisita) {
@@ -25,10 +27,20 @@
                     data: {idVisita: idVisita},
                     success: function(datos) {
                         visitaImprimir.callbacks.agregarNotas(datos);
+                    }
+                });
+            },
+            obtenerDatosVisita: function(idVisita) {
+                $.ajax({
+                    type: 'GET',
+                    url: "vehiculos/_actions/_imprimirVisita.php",
+                    contentType: 'json',
+                    dataType: 'json',
+                    cache: false,
+                    data: {idVisita: idVisita},
+                    success: function(datos) {
+                        visitaImprimir.callbacks.agregarValoresImpresion(datos);
                         
-                    },
-                    error: function() {
-
                     }
                 });
             }
@@ -39,6 +51,33 @@
                     $('#imprimirNotas').append(' - ' +arrDatos[i].descripcion + ' <br/>');
                 }
             },
+            agregarValoresImpresion: function(arrDatos) {
+                $("#imprimirNombre").html(arrDatos.nombres);
+                $("#imprimirApellido").html(arrDatos.apellidos);
+                $("#imprimirRUN").html(arrDatos.run);
+                $("#imprimirCorreo").html(arrDatos.correo);
+                $("#imprimirCalle").html(arrDatos.calle);
+                $("#imprimirPoblacion").html(arrDatos.poblacion);
+                $("#imprimirNumeroDomicilio").html(arrDatos.numeroDomicilio);
+                $("#imprimirDptoBlock").html(arrDatos.depto);
+                $("#imprimirTelefonoCelular").html(arrDatos.telefonoCelular);
+                $("#imprimirTelefonoFijo").html(arrDatos.telefonoFijo);
+                $("#ImprimirRegion").html(arrDatos.nombreRegion);
+                $("#imprimirComuna").html(arrDatos.nombreComuna);
+                $("#imprimirMarca").html(arrDatos.nombreMarca);
+                $("#imprimirModelo").html(arrDatos.modelo);
+                $("#imprimirPatente").html(arrDatos.patente);
+                $("#imprimirAnio").html(arrDatos.anio);
+                $("#imprimirKilometrajeInicio").html(arrDatos.kilometrosInicio);
+                $("#imprimirVIN").html(arrDatos.vin);
+                $("#imprimirKilometrajeVisita").html(arrDatos.kilometrosVisita);
+                $("#imprimirFechaIngreso").html(arrDatos.fechaIngreso);
+                $("#imprimirOT").html(arrDatos.ot);
+                $("#imprimirDescripcion").html(arrDatos.imprimirDescripcion);
+            },
+            limpiarFichaImpresion: function(){
+                //limpiar los datos
+            }
         }
     };
     visitaImprimir.ui.init();
